79d293ad94149329e700a48042839b0d09b4240f
subajat1 <bayumunajat@outlook.com>
2019-05-24 23:01:43 +0800

    develop props

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -5,7 +5,23 @@ export class Home extends React.Component {
         return (
             <div>
                 <p>In a new Component!</p>
+                <p>Name : {this.props.name}</p>
+                <div>
+                    <h4>Hobbies</h4>
+                    <ul>
+                        { this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>) }
+                    </ul>
+                </div>
+                <hr/>
+                {this.UNSAFE_componentWillMount.props.children}
             </div>
         );
     }
 }
+
+Home.propTypes = {
+    name: React.PropTypes.string,
+    age: React.PropTypes.number,
+    user: React.PropTypes.object,
+    children: React.PropTypes.element.isRequired
+};
--- src/app/index.js
+++ src/app/index.js
@@ -7,6 +7,10 @@ import { Home } from "./components/Home";
 class App extends React.Component {
 
     render() {
+        var user = {
+            name: 'Anna',
+            hobbies: ['sports', 'reading']
+        };
         return (
             <div className="container">
                 <div className="row">
@@ -16,7 +20,9 @@ class App extends React.Component {
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home/>
+                        <Home name={"Max"} age={27} user={user}>
+                            <p>childres are here!</p>
+                        </Home>
                     </div>
                 </div>
             </div>
